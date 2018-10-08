bbe54601b4ea04d57e6ab3fa4fefcf1dabb7aacc
Aleksandra <al.byczynska@gmail.com>
2018-10-09 00:01:41 +0200

    empty initial files

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    import sys
+
+    print("This is the name of the script: ", sys.argv[0])
+
+    # replace with some lib for args (argparse) and read how many nodes each layer should have
+    for ar in sys.argv:
+        print(ar)
+
+    main()
