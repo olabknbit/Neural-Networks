eb8777133954f79c5096172ef24986097c332cb2
Aleksandra <al.byczynska@gmail.com>
2018-10-29 23:00:42 +0100

    fix recall syn

--- hopfield/main.py
+++ hopfield/main.py
@@ -5,19 +5,18 @@ def main():
     parser.add_argument('--train_filename', type=str, help='Name of a file containing training data', required=True)
     parser.add_argument('-w','--width', type=int, help='Image width', required=True)
     parser.add_argument('-l','--length',  type=int, help='Image length', required=True)
+    parser.add_argument('--seed', type=int, help='Random seed int', required=False, default=1)
+    parser.add_argument('--flip', type=int, help='How many bits to flip while testing', required=False, default=5)
+    parser.add_argument('--visualize', type=bool, help='Generate image after each run', required=False, default=False)
 
     args = parser.parse_args()
 
-    from util import print_image, read_file
+    from util import read_file
     images = read_file(args.train_filename)
 
-    # print(args.width)
-    # print(args.length)
-    # for image in images:
-    #     print_image(image, args.width, args.length)
-
     import hopfield_net
-    hopfield_net.run(images, args.width, args.length)
+    hopfield_net.run(images, args.width, args.length, args.seed, args.flip, args.visualize)
+
 
 if __name__ == "__main__":
     main()
