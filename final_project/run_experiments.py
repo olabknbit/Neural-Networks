89e18e5e47a602f368b1a5084f40d5b78940368b
Aleksandra <al.byczynska@gmail.com>
2018-12-04 16:57:05 +0100

    fix rebase errors

--- final_project/run_experiments.py
+++ final_project/run_experiments.py
@@ -10,9 +10,11 @@ def generate_data(routes_lines, routes_stops, hours_n, day_length, hours_buses, 
                                                        hours_buses)
     test_data_filename = util.get_test_data_filename(routes_lines, routes_stops, trips_m, trips_transfers, hours_n,
                                                      hours_buses)
-    accuracy = regression.main(train_data_filename, test_data_filename,
+    from perceptron.util import tanh, tanh_derivative
+    accuracy = regression.main(train_data_filename, test_data_filename, activation_f=tanh,
+                               activation_f_derivative=tanh_derivative,
                                create_nn=[3], save_nn=None, read_nn=None, number_of_epochs=10000, visualize_every=None,
-                               l_rate=0.001, biases=True)
+                               l_rate=0.001, biases=True, savefig_filename=None)
     print('accuracy', accuracy)
 
 
