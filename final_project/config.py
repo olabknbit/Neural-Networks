6e82228a2d7dba55b396e4414935120fd8049354
Michał Omelańczuk <omelanczukm@student.mini.pw.edu.pl>
2019-01-08 04:23:59 +0100

    Fix

--- final_project/config.py
+++ final_project/config.py
@@ -1,10 +1,10 @@
 # file names params
-routes_lines = 1
+routes_lines = 5
 routes_stops = 6
-hours_n = 50 # how many rows of train and test data to generate (split 60:40)
-hours_buses = [1]  # how many buses of each line should run
-trips_transfers = 0  # max how many transfers each passenger can have
-trips_m = 15  # how many passenger's trips there are
+hours_n = 80 # how many rows of train and test data to generate (split 60:40)
+hours_buses = [1,2,3,1,1]  # how many buses of each line should run
+trips_transfers = 2  # max how many transfers each passenger can have
+trips_m = 20  # how many passenger's trips there are
 
 # NEAT params
 COMPATIBILITY_THRESHOLD = 2.5
@@ -18,14 +18,13 @@ ADD_NEURON_MUTATION_CHANCE = 0.3
 n_iter = 10
 l_rate = 0.001
 
-n_generations = 5
+n_generations = 4
 
 # test params
 neat_tests = 100
-monte_carlo_tests_small = 101
-monte_carlo_tests_medium = 500
-monte_carlo_tests_big= 1000
-
+monte_carlo_tests_small = 1010
+monte_carlo_tests_medium = 5000
+monte_carlo_tests_big= 10000
 
 def get_filenames_params():
     return routes_lines, routes_stops, hours_n, hours_buses, trips_m, trips_transfers
--- final_project/test_neat.py
+++ final_project/test_neat.py
@@ -35,19 +35,27 @@ def test_neat(neural_network):
     trips_filename = util.get_trips_filename(routes_lines, routes_stops, trips_m, trips_transfers)
     hours_filename = util.get_hours_results_filename(hours_n, hours_buses) 
 
+    i = 0
+    # with open(hours_filename, 'w') as file:
+    #     for _, hours in enumerate(best_results):
+    #         print(hours[0])
+    #         file.write(str(hours[0]))
+    #         file.write('\n')
 
     with open(hours_filename, 'w') as file:
-        for _ in range(number_of_tests):
+        for i in range(bests):
             hours = []
+           
             for bus in hours_buses:
+                k=0
                 line_hours = []
-                for _ in range(bus):
-                    line_hours.append(random.randint(-util.DAY_LENGTH/2 , util.DAY_LENGTH/2))
+                for j in range(bus):
+                    line_hours.append(best_results[i][0][bus+k])
+                    k=k+1
                 hours.append(line_hours)
-            tests.append(hours)
+
             file.write(str(hours))
             file.write('\n')
-
     
     import simulator
 
